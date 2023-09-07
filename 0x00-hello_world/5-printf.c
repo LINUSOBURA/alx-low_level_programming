d638e8badb639af28f0bc4bfa7414f3099cd7724
LINUSOBURA <linusobura@gmail.com>
2023-09-07 09:00:45 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - functio block for our code
+ *
+ * Return: (0) for sucess
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
