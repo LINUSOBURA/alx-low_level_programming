0f33e4e873fae05b9cb95c44119757b317c1f030
LINUSOBURA <linusobura@gmail.com>
2023-09-08 04:14:19 -0700

    alpHABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main-Entrypointofcode
+ *
+ * Return:(0)sucess
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
