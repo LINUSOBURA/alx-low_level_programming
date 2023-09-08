8a20d0747c3b4f97292564ce89e8ea047bae86e9
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-09-08 13:39:26 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,15 +2,15 @@
 
 /**
  * main - Entry point of code
- * 
+ *
  * Return: (0) sucess
  */
 
 int main(void)
 {
     char c;
-    
-    for (c='a';c<='z';c++)
+
+    for (c='a'; c<='z'; c++)
         putchar(c);
 
     putchar('\n');
