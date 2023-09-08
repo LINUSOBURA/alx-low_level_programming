a2b0fbc225840f8c0583a713cb16baa8950ea9fd
LINUSOBURA <linusobura@gmail.com>
2023-09-08 03:36:56 -0700

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of code
+ * 
+ * Return: (0) sucess
+ */
+
+int main(void)
+{
+    char c;
+    
+    for (c='a';c<='z';c++)
+        putchar(c);
+
+    putchar('\n');
+    return (0);
+}
