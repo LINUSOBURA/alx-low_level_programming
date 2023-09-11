47438743dcc93f6daf430483f2269045e09c6815
LINUSOBURA <linusobura@gmail.com>
2023-09-11 08:11:30 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point of our code
+ * Return: 1 for success
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19");
+	return (1);
+}
