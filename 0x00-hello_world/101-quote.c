0d872f7145a647b2365f46da623f9d8a7c722632
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-09-11 18:24:50 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,6 @@
-#include <stdio.h>
-#include <unistd.h>
-/**
- * main - entry point of our code
- * Return: 1 for success
- */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 58);
-	return (1);
+    fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
+    return (1);
 }
