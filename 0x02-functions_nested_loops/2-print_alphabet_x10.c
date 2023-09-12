0f5c447433e5e5047f5694549da79e7d7654276e
LINUSOBURA <linusobura@gmail.com>
2023-09-12 08:39:52 -0700

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - main function
+ */
+void print_alphabet_x10(void)
+{
+	int l = 0;
+	int i;
+
+	while (l <= 9)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+	l++;
+}
