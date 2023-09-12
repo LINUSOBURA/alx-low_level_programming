bf1052bacb6d3236f1467f1d8a12a078ff69bef9
LINUSOBURA <linusobura@gmail.com>
2023-09-12 10:15:10 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Entry point of the program
+ * @int: parameter
+ * Return: 0 sucess
+ */
+int _abs(int)
+{
+	if (int < 0)
+	{
+		int = int * -1;
+	}
+	return (0);
+}
