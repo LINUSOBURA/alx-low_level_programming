86356cde77273905288d7037a62b7f16c2c0bb91
LINUSOBURA <linusobura@gmail.com>
2023-09-12 10:38:43 -0700

    lastnumber

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - last digit of a number
+ * @i: number to be checked
+ * Return: 0 sucess
+ */
+int print_last_digit(int i)
+{
+	int lastdigit;
+
+	lastdigit = i % 10;
+	return (lastdigit);
+}
