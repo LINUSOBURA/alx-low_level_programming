e8787d10e495a86d6d6826ab6256fda3f7ef09b4
LINUSOBURA <linusobura@gmail.com>
2023-09-12 09:05:36 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - function to check is a number is lcse
+ * @c:  parameter to be checked in ASCII
+ * Return: 0 sucess
+ */
+int _islower(int c);
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
