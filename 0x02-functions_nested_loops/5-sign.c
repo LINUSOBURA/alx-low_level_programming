c8c92c8514e3c218af202bc527a110df572a5336
LINUSOBURA <linusobura@gmail.com>
2023-09-12 09:46:52 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - Entry point of our code
+ * @n: number to be checked
+ * Return: 0 if zero 1 if + -1 if -
+ */
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
