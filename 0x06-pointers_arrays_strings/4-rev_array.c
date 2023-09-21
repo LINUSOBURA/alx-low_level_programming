6030766e0f2538a41b86500cd71d535a199e717c
LINUSOBURA <linusobura@gmail.com>
2023-09-21 04:38:51 -0700

    Reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - function to reverse an array
+ * @a: the array to be reversed
+ * @n: number of elements i the array
+ * Return: nothing
+ * Code by Linus Obura
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+
+	for (i = 0; i <= n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
