ac6032f63899fd0af524509aff8ebfc368972cfd
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-09-21 21:28:23 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
-#include <string.h>
 /**
  * leet - function to change characters to numbers
  * @str: string of characters
@@ -9,23 +7,21 @@
  */
 char *leet(char *str)
 {
-	int i, j;
-	char *leet_str = malloc(strlen(str) + 1);
+	char from[] = "aeotlAEOTL";
+	char to[] = "4307143071";
+	int i = 0;
+	int j;
 
-	for (i = 0, j = 0; str[i] != '\0'; i++)
+	while (*(str + i) != '\0')
 	{
-		if (str[i] == 'a' || str[i] == 'A' || str[i] == 'e' ||
-				str[i] == 'E' || str[i] == 'o' || str[i] == 'O'
-				|| str[i] == 't' || str[i] == 'T' ||
-				str[i] == 'l' || str[i] == 'L')
+		for (j = 0; j <= 9; j++)
 		{
-			leet_str[j++] = str[i] - 'a' + '4';
+			if (*(str + i) == from[j])
+			{
+				*(str + i) = to[j];
+			}
 		}
-		else
-		{
-			leet_str[j++] = str[i];
-		}
+		i++;
 	}
-	leet_str[j] = '\0';
-	return (leet_str);
+	return (str);
 }
