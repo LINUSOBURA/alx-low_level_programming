f140d12313bd348e0e875028a4cb911a20a4792b
LINUSOBURA <linusobura@gmail.com>
2023-09-25 10:58:20 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strbrk - string that searches a string for any set of bytes
+ * _strpbrk - string that searches a string for any set of bytes
  * @s: strig to be searched upon
  * @accept: string to be searched
  * Return: pointer to the byte in s that is in accept
