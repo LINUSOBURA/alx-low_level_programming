f5834adbb6dfd116d041c91fc853a29ba25cda6c
LINUSOBURA <linusobura@gmail.com>
2023-09-25 12:51:07 -0700

    chess board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,16 +1,13 @@
 #include "main.h"
-/**
-  * _chessboard - function to print the chessboard
-  * @a: pointer to an array
-  */
-void print_chessboard(char (*a)[8])
-{
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar('\n');
-	}
+void print_chessboard(char (*a)[8]) {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            // Assuming 'a' is a 2D array representing the chessboard, 
+            // print each character from the array.
+            // Use a simple custom putchar function to display the character.
+            putchar(a[i][j]);
+        }
+        // Print a newline after each row using the same custom putchar function.
+        putchar('\n');
+    }
 }
