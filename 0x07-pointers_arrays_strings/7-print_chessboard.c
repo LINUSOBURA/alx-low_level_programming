fad4806611359b655b36696d7779f236a2403df3
LINUSOBURA <linusobura@gmail.com>
2023-09-25 13:02:48 -0700

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,17 @@
 #include "main.h"
+/**
+ * print_chessboard - function to print cessboard
+ * @a: pointer to an array
+ * Code by Linus Obura
+ */
 
 void print_chessboard(char (*a)[8])
 {
-	for (int i = 0; i < 8; i++)
+	int i, j;
+
+	for (i = 0; i < 8; i++)
 	{
-		for (int j = 0; j < 8; j++)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
