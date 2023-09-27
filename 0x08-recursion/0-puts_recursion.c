ab2f8a286fd4840afcc991ed14c6b8cc18f11f3a
LINUSOBURA <linusobura@gmail.com>
2023-09-27 05:51:08 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts_recursion - Funtion that prints a string
+ * @s: string to be printed
+ * Code by Linus Obura
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+
+	}
+	_putchar('\n');
+
+}
