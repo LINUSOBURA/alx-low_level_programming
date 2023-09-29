27368525ada1424c0bed7acaea958916d3884396
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-09-29 23:49:27 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,18 +15,18 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	
+
 	int cents = atoi(argv[1]);
-	
+
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	
+
 	int coins[] = {25, 10, 5, 2, 1};
 	int num_coins = 0;
-	
+
 	for (int i = 0; i < 5 && cents > 0; i++)
 	{
 		while (cents >= coins[i])
@@ -35,8 +35,8 @@ int main(int argc, char *argv[])
 			num_coins++;
 		}
 	}
-	
+
 	printf("%d\n", num_coins);
-	return 0;
+	return (0);
 }
 
