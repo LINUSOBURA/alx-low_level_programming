192f402f7789b8a1682f6aafe4c3e9cd544f1a2c
LINUSOBURA <linusobura@gmail.com>
2023-09-29 13:22:28 -0700

    Add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cytpe.h>
 /**
  * main - entry point of the funtion
  * @argc: arguments count
@@ -7,26 +8,35 @@
  * Return: 0
  * Code by Linus Obura
  */
+int is_positive_number(char *str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        // Check if the character is not a digit
+        if (!isdigit(str[i])) {
+            return 0;  // false
+        }
+    }
+    return 1;  // true
+}
+
 int main(int argc, char *argv[])
 {
-	int i, add = 0;
+    int i, add = 0;
 
-	if (argc < 2)
-	{
-		printf("0\n");
-	}
+    if (argc < 2) {
+        printf("0\n");
+        return 0;
+    }
 
-	for (i = 1; i < argc; i++)
-	{
-		if (atoi(argv[i]) == 0 && argv[i][0] != '0')
-		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-			add = add + atoi(argv[i]);
+    for (i = 1; i < argc; i++) {
+        if (!is_positive_number(argv[i])) {
+            printf("Error\n");
+            return (1);
+        }
+        else
+            add += atoi(argv[i]);
+    }
 
-	}
-	printf("%d\n", add);
-	return (0);
+    printf("%d\n", add);
+    return (0);
 }
+
