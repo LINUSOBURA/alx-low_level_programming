94e05bc03ed524d70d79a0c640d079fb4ae528c1
LINUSOBURA <linusobura@gmail.com>
2023-09-29 04:48:00 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point of the funtion
+ * @argc: arguments count
+ * @argv: arguments string
+ * Return: 0
+ * Code by Linus Obura
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	argc = argc - 1;
+	printf("%d\n", argc);
+	return(0);
+}
