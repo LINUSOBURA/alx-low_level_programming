56ed4261787a6f9feddcdb5d760612219d1475d3
LINUSOBURA <linusobura@gmail.com>
2023-09-29 12:45:07 -0700

    Add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point of the funtion
+ * @argc: arguments count
+ * @argv: arguments string
+ * Return: 0
+ * Code by Linus Obura
+ */
+int main(int argc, char *argv[])
+{
+	int i, add = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0 && argv[i][0] != '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			add = add + atoi(argv[i]);
+
+	}
+	printf("%d\n", add);
+	return (0);
+}
