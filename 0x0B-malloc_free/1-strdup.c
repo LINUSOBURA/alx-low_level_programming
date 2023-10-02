fe67690ad3f9078a8f73ab8ff3a488df03b73cdc
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-02 13:44:18 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,12 +9,15 @@
  */
 char *_strdup(char *str)
 {
-	size_t length = strlen(str) + 1;
-
-	char *New_string = (char *) malloc(length * sizeof(char));
+	size_t length;
+	char *New_string;
 
 	if (str == NULL)
 		return (NULL);
+	
+	length = strlen(str) + 1;
+
+	New_string = (char *) malloc(length * sizeof(char));
 
 	if (New_string != NULL)
 		strcpy(New_string, str);
