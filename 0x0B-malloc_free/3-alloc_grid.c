7e727943f842d9cffd697abf2acd8fb34a546aaf
LINUSOBURA <linusobura@gmail.com>
2023-10-02 08:37:09 -0700

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - function that returns a pointer to 2D array
+ * @width: grid width
+ * @height: grid height
+ * Return: Null or pointer
+ * Code by Linus Obura
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, **arr;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (0);
+	}
+
+	arr = (int **) malloc(height * sizeof(int *));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *) malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+
+	return (arr);
+}
