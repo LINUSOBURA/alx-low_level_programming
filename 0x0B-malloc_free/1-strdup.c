bbf825e41cdf3f082f4ba0d627a656f13863b297
LINUSOBURA <linusobura@gmail.com>
2023-10-02 03:13:35 -0700

    Duplicate strings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup- Function to return a pointer to duplicate string
+ * @str: pointer to a string to be duplicated
+ * Return: Null or pointer to duplicated string
+ * Code by Linus Obura
+ */
+char *_strdup(char *str)
+{
+	size_t length = strlen(str) + 1;
+
+	char *New_string = (char *) malloc(length * sizeof(char));
+
+	if (str == NULL)
+		return (NULL);
+	if (New_string == NULL)
+		return (NULL);
+
+	strcpy(New_string, str);
+
+	return (New_string);
+
+}
