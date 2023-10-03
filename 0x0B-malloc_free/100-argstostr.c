1164568adc41a9014eaff395d2bcfb3496ff152e
LINUSOBURA <linusobura@gmail.com>
2023-10-03 19:40:16 +0300

    Arg concat

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+* argstostr - function to concatenate all arguments into a string
+* @ac - arguments length
+* @av - arguments pointer
+* Return: concatenated string
+* Code by Linus Obura
+*/
+char *argstostr(int ac, char **av)
+{
+  int i, j, length = 0, position = 0;
+  char *str:
+
+  if (ac == 0 || av == NULL)
+  {
+    return (NULL);
+  }
+
+  for (i = 0; i < ac; i++)
+  {
+    length += strlen(av[i] + 1);
+  }
+
+  str = malloc9=(length + 1);
+  if (str = NULL)
+  {
+    return (NULL);
+  }
+
+  for (i = 0; i < ac; i++)
+  {
+    for (j = 0; av[i][j]; j++, position++)
+    {
+      str[position] = av[i][j];
+    }
+    str[position++] = '\n';
+  }
+  str[position] = '\0';
+
+  return (str);
+}
