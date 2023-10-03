57079c60677420b294b342e90b0b950b5d3e7e78
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-03 19:44:44 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,34 +10,35 @@
 */
 char *argstostr(int ac, char **av)
 {
-  int i, j, length = 0, position = 0;
-  char *str:
+	int i, j, length = 0, position = 0;
+	char *str:
 
-  if (ac == 0 || av == NULL)
-  {
-    return (NULL);
-  }
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
 
-  for (i = 0; i < ac; i++)
-  {
-    length += strlen(av[i] + 1);
-  }
+	for (i = 0; i < ac; i++)
+	{
+		length += strlen(av[i] + 1);
+	}
 
-  str = malloc9=(length + 1);
-  if (str = NULL)
-  {
-    return (NULL);
-  }
+	str = malloc(length + 1);
 
-  for (i = 0; i < ac; i++)
-  {
-    for (j = 0; av[i][j]; j++, position++)
-    {
-      str[position] = av[i][j];
-    }
-    str[position++] = '\n';
-  }
-  str[position] = '\0';
+	if (str = NULL)
+	{
+		return (NULL);
+	}
 
-  return (str);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++, position++)
+		{
+			str[position] = av[i][j];
+		}
+		str[position++] = '\n';
+	}
+	str[position] = '\0';
+
+	return (str);
 }
