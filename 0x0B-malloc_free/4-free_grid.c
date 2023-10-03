b9c4bd1451d0020cba87b146ed105061b5ada137
LINUSOBURA <linusobura@gmail.com>
2023-10-03 18:45:22 +0300

    Free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* free_grid - function to free the memmory allocated to grid function
+* @grid: Grid array
+* @height: array size
+* Rerutn: nothing
+* Code by Linus Obura
+*/
+void free_grid(int **grid, int height)
+{
+  int i;
+
+  if (grid == NULL)
+  {
+    return;
+  }
+
+  for (i = 0; i < height; i++)
+  {
+    free(grid[i]);
+  }
+
+  free(grid);
+}
