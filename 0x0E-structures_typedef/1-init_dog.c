7e2f79fbf96a6a08e28c608eecd558b14bc1a87f
LINUSOBURA <linusobura@gmail.com>
2023-10-09 10:12:03 -0700

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - function to initialize dog struct
+ * @d: dog struct
+ * @name: dog name
+ * @age: dog age;
+ * @owner: dog owner
+ * Code by Linus Obura
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
