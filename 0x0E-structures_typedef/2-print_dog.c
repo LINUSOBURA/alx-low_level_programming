26fbfd2089d0688d6751aa78b95d37994a5e1e4d
LINUSOBURA <linusobura@gmail.com>
2023-10-09 11:02:14 -0700

    print

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - function to print dog struct
+ * @d: the dog to be printed
+ * Code by Linus Obura
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
