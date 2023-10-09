9b7accbb8518eff3ba4c0ed6ac99eafad86ccc05
LINUSOBURA <linusobura@gmail.com>
2023-10-09 08:41:57 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog 
+/**
+ * struct dog - structure for dog
+ * @name: name pointer
+ * @age: age float
+ * @owner: owner pointer
+ */
+struct dog
 {
-	char *name; 
-	float age; 
+	char *name;
+	float age;
 	char *owner;
 };
 #endif
