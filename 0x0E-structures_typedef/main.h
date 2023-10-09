413c9ab209d1e3afbcc15137c4852bc88ef1919d
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-09 19:18:19 +0300

    Update main.h

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -3,6 +3,6 @@
 void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
+void dog_t *new_dog(char *name, float age, char *owner);
 int _putchar(char c);
 #endif
