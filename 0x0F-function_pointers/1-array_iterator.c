a090bb97591785cc50abaec80226173235fc8c85
LINUSOBURA <linusobura@gmail.com>
2023-10-11 19:15:39 +0300

    array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,9 @@
 #include "function_pointers.h"
 /**
-* @array: 
+ * array_iterator - function that execute a function given a parameter
+* @array: array of pointer function
+* @size: size of the array
+* @action: pointer to a function
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
