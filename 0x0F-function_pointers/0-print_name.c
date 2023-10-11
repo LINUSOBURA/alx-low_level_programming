6e5f0b9a37cb4c2897f3f156c608af8d3cd8f262
LINUSOBURA <linusobura@gmail.com>
2023-10-11 17:13:03 +0300

    Initial commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,23 @@
+#include "function_pointer.h"
+/**
+* print_name - function that prints a name;
+* @name: pointer to a string name
+* @f: pointer to a function
+* Code by Linus Obura
+*/
+
+void f(char c);
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
+/**
+* f - function to print a name
+* @c: character to be printed
+*/
+void f(char c)
+{
+	printf(c);
+}
