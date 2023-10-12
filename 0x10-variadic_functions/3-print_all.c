868e02bb23da9fea68e449808bc44e97d06dd2dc
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-13 00:20:53 +0300

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,10 +9,6 @@
 void print_all(const char * const format, ...)
 {
 	unsigned int i = 0, comma = 0;
-	char c_value;
-	int i_value;
-	float f_value;
-	char *s_value;
 	va_list params;
 
 	va_start(params, format);
@@ -26,28 +22,24 @@ void print_all(const char * const format, ...)
 		switch (format[i++])
 		{
 			case 'c':
-				c_value = (char)va_arg(params, int);
-				printf("%c", c_value);
+				printf("%c", (char)va_arg(params, int));
 				comma = 1;
 				break;
 			case 'i':
-				i_value = va_arg(params, int);
-				printf("%d", i_value);
+				printf("%d", va_arg(params, int));
 				comma = 1;
 				break;
 			case 'f':
-				f_value = (float)va_arg(params, double);
-				printf("%f", f_value);
+				printf("%f", (float)va_arg(params, double));
 				comma = 1;
 				break;
 			case 's':
-				s_value = va_arg(params, char *);
-				if (!s_value)
+				if (!va_arg(params, char *))
 				{
 					printf("(nil)");
 					break;
 				}
-				printf("%s", s_value);
+				printf("%s", va_arg(params, char *));
 				comma = 1;
 				break;
 			default:
