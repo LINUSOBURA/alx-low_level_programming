07d31b0620050a016c40d0fefee3c6f576b70375
LINUSOBURA <linusobura@gmail.com>
2023-10-12 06:04:34 -0700

    sum it all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,13 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	unsigned int sum = 0;
+	va_list parameters;
+	va_start(parameters, n);
 
 	if (n == 0)
 	{
 		return (0);
 	}
-
-	va_list parameters;
-	va_start(parameters, n);
 
 	for (i = 0; i < n; i++)
 	{
