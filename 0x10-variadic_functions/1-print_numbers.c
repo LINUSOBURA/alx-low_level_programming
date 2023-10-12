35370c78f48a13a8057189621b8d6c98abdf999e
LINUSOBURA <linusobura@gmail.com>
2023-10-12 07:35:16 -0700

    print ints

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: separator for numbers
+ * @n: number of ints passed to function
+ * Code by Linus Obura
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list param;
+	unsigned int value;
+
+	va_start(param, n);
+
+	for (i = 0; i < n; i++)
+	{
+		value = va_arg(param, unsigned int);
+		if (separator == NULL)
+			separator = "";
+		printf("%d", value);
+
+		if (i != n - 1)
+			printf("%c ", *separator);
+	}
+
+	printf("\n");
+
+	va_end(param);
+}
