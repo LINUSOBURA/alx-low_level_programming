8ef46ac1858f478fb0e8491bd279905619846aac
LINUSOBURA <linusobura@gmail.com>
2023-10-19 11:43:34 -0700

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *createNewNode(char *str) = (list_t *)malloc(sizeof(Node));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->str = str;
+	newnode->next = NULL;
+	return (newNode);
+
+
+
+}
