e516409d3b44c7661bb2e87e5775c2778bda33d0
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-19 22:01:16 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,28 @@
 #include "lists.h"
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *createNewNode(char *str) = (list_t *)malloc(sizeof(Node));
+	list_t *newNode = createNewNode(str);
 	if (newNode == NULL)
 		return (NULL);
-	newNode->str = str;
-	newnode->next = NULL;
+	
+	newNode->next = *head;
+	*head = newNode;
 	return (newNode);
+}
 
+list_t *createNewNode(char *)
+{
+	list_t *newNode = (list_t *)malloc(sizeof(list_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->str = strdup(str);
+	if (newNode->str == NULL)
+	{
+		free(newNode);
+		return (newNode);
+	}
 
-
+	newNode->len =strlen(str);
+	newNode->next = NULL;
+	return newNode;
 }
