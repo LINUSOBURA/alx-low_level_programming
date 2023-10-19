1d93da52ab85e3872c732488ea049ddef83543d2
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-19 19:06:02 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,17 +3,18 @@
 #include <stddef.h>
 #include <stdio.h>
 /**
-* struct list_s - singly liked list
-* @len: integer
-* @next: pointer to the next element in the list
-* Description: singly linked list node structure
-* Code by Linus Obura
-*/
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
