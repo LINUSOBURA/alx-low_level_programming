548d96938fc7eca9ca0bedb32a345b4dfec975d8
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-19 18:46:07 +0300

    lists update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,10 +2,11 @@
 #define LISTS_H
 #include <stddef.h>
 #include <stdio.h>
-typedef struct list_s {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
