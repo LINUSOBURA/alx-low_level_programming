6470352dd59a85794674b2552c999ebc32c96a04
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-24 19:49:10 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+/**
+* get_nodeint_at_index - function to get the value at index nth index
+* @head: the first node of the list
+* @index: the index of the value to be returned
+* Return: calue at nth index
+* Code by Linus Obura
+*/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *current = head;
