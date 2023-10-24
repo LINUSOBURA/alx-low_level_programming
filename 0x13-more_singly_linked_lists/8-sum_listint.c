4513379689e615bfadc8736f739c2a993aeae570
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-25 00:41:21 +0300

    sum

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+* sum_listint - function to sum up all data in the list
+* @head: pointer to the first node in the list
+* Return: sum
+* Code by Linus Obura
+*/
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
