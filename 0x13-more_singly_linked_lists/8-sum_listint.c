0dde7210ea0bbfa7d258c7bb4a8e47e80b147dfd
LINUSOBURA <linusobura@gmail.com>
2023-10-24 14:36:08 -0700

    sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *current = head;
+
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (sum);
+}
