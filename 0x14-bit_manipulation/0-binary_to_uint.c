1d674ead10ffeccd47df239ea39c1a6bdfacd735
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-26 16:17:14 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,11 +7,11 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
-	int length = strlen(b), i;
-
 	if (b == NULL)
 		return (0);
+
+	unsigned int decimal = 0;
+	int length = strlen(b), i;
 
 	for (i = 0; i < length; i++)
 	{
