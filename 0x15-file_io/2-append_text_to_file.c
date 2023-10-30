149395f83675e396b5b72b0a3d4dd4d577658116
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-30 23:34:20 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,10 +14,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	file_descriptor = open(filename, O_WRONLY | O_APPEND);
-	
+
 	if (file_descriptor == -1)
-        return (-1);
-	
+		return (-1);
+
 	if (text_content != NULL)
 	{
 		content_length = strlen(text_content);
@@ -29,7 +29,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
-	
+
 	close(file_descriptor);
 	return (1);
 }
