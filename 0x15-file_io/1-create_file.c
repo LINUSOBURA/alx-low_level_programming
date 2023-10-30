7f971ae0535c4b4add1e54511038711632390e78
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2023-10-30 23:07:05 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,8 +17,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file_descriptor = open(filename, 0_CREAT | 0_WRONLY
-			| 0_TRUNC, S_IRUSR | S_IWUSR);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY
+			| O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_descriptor == -1)
 		return (-1);
 
