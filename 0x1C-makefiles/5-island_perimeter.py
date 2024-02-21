adeed9b61de8966da14e5e031f8ef72fcc678f5d
Linus Obura <56161418+LINUSOBURA@users.noreply.github.com>
2024-02-21 23:29:37 +0300

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,5 +16,5 @@ def island_perimeter(grid):
                     x, y = i + dx, j + dy
                     if x < 0 or x >= rows or y < 0 or\
                         y >= cols or grid[x][y] == 0:
-                        perimeter += 1
+                            perimeter += 1
     return perimeter
